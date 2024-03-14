beb8c65fd871621e307213d644b121273476d662
hvacTech0302 <mb030287@proton.me>
2024-03-13 20:32:16 -0400

    completed Project 3 -- Star Rating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Accordion from "./components/accordion/accordion.component";
+import RandomColor from "./components/random-color/random-color.component";
+import StarRating from "./components/star-rating/star-rating.component";
 
 function App() {
   return (
     <div className="App">
       {/* Accordion Component */}
       <Accordion />
+      {/* Random Color Component */}
+      <RandomColor />
+      {/* Star Rating Component */}
+      <StarRating numOfStars={10} />
     </div>
   );
 }
